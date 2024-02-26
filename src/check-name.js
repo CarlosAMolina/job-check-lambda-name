692aa06c60978cb2145a4252898ed597ec750f69
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2024-02-26 01:50:31 +0100

    Add functions

--- src/check-name.js
+++ src/check-name.js
@@ -1,12 +1,28 @@
-function sum_1(number) {
-  return number + 1;
+const MAX_ALLOWED_LENGTH = 86; // TODO use corret value
+
+function show_string_analysis(string) {
+  console.log(`Checking string: ${string}`);
+  console.log(`Lenght of the string: ${string.length}`);
+  const modified_string = get_string_modified_by_deploy_pipe(string);
+  console.log(`String modified by the deployment pipeline: ${modified_string}`);
+  console.log(`Lenght of the string modified: ${modified_string.length}`);
+  console.log(`Maximum allowed lenght: ${MAX_ALLOWED_LENGTH}`);
+  const is_valid = has_allowed_size(modified_string);
+  console.log(`Is a valid string: ${is_valid}`);
 }
 
-function sum_numbers(number1, number2) {
-  return number1 + number2;
+function get_string_modified_by_deploy_pipe(string) {
+  const PREFIX_ADDED = "Deploy-"; // TODO use corret value
+  const SUFFIX_ADDED = "-dev"; // TODO use corret value
+  return `${PREFIX_ADDED}${string}${SUFFIX_ADDED}`;
+}
+
+function has_allowed_size(string) {
+  return string.length <= MAX_ALLOWED_LENGTH;
 }
 
 export const exportedForTesting = {
-  sum_1,
-  sum_numbers,
+  show_string_analysis,
+  get_string_modified_by_deploy_pipe,
+  has_allowed_size,
 };
--- test/check-name.test.js
+++ test/check-name.test.js
@@ -2,8 +2,27 @@ import { describe, expect, it } from "@jest/globals";
 
 import { exportedForTesting } from "../src/check-name.js";
 
-describe("Check function sum_1", function () {
-  it("Check expected result", function () {
-    expect(exportedForTesting.sum_1(1)).toBe(2);
+describe("Check function show_string_analysis", function () {
+  it("Check runs without error", function () {
+    expect(exportedForTesting.show_string_analysis("foo"));
+  });
+});
+
+describe("Check function get_string_modified_by_deploy_pipe", function () {
+  it("Check expected value", function () {
+    expect(exportedForTesting.get_string_modified_by_deploy_pipe("foo")).toBe(
+      "Deploy-foo-dev",
+    );
+  });
+});
+
+describe("Check function has_allowed_size", function () {
+  it("Check is true", function () {
+    const string = "a".repeat(86);
+    expect(exportedForTesting.has_allowed_size(string)).toBe(true);
+  });
+  it("Check is false", function () {
+    const string = "a".repeat(87);
+    expect(exportedForTesting.has_allowed_size(string)).toBe(false);
   });
 });
